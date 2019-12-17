c3fa4a73eb1ade466273a4c255c56f1c143a7957
Brian Zauner <binrz00@hotmail.com>
2019-12-17 10:07:25 -0600

    attempting to use their code

---
+++ Develop/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+
+//set up express
+const app = express();
+const PORT = process.env.PORT||3000;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// send the user to the correct html using routes
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
