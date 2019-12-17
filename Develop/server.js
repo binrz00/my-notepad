8aa1ca6ce58487f92e728cd995fed7a11dd8f2cd
Brian Zauner <binrz00@hotmail.com>
2019-12-17 17:39:43 -0600

    got css to load

--- Develop/server.js
+++ Develop/server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 
 //set up express
 const app = express();
 const PORT = process.env.PORT||3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname + '/public'));
 // send the user to the correct html using routes
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "/public/index.html"));
@@ -13,6 +15,7 @@ app.get("/", function(req, res) {
 app.get("/notes", function(req, res) {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
+
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
