9609f2c6a3904fea1a8a710bf9d88748210cd09b
Brian Zauner <binrz00@hotmail.com>
2019-12-17 18:38:48 -0600

    req dot body

--- Develop/server.js
+++ Develop/server.js
@@ -10,12 +10,17 @@ app.use(express.json());
 app.use(express.static(__dirname + '/public'));
 // send the user to the correct html using routes
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "/public/index.html"));
+  res.sendFile(path.join(__dirname, "/public/index.html"))
 });
 app.get("/notes", function(req, res) {
-  res.sendFile(path.join(__dirname, "/public/notes.html"));
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
 });
-
+app.get("/api/notes",function(req,res){
+  res.sendfile(path.join(__dirname, "/db/db.json"))
+});
+app.post("api/notes",function(req,res){
+  req.body.sendfile(paht.join(__dirname, "db/db.json"))
+});
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
